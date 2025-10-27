a6b073948cdf16e410a3e63112cd6244c1197029
makbulhosen5000 <makbulhosen5000@gmail.com>
2025-10-27 16:49:02 +0600

    feat:install nextJs and shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>Navbar</nav>
         {children}
+        <footer>Footer</footer>
       </body>
     </html>
   );
